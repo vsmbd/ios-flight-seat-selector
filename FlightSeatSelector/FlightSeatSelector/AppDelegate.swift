fe1d2968411214498022a85528d658ef1d1b91a3
vsmbd <vsmbd42f29@privaterelay.appleid.com>
2026-02-10 20:01:23 +0530

    Add Telme integration and new dependencies in AppDelegate; update project configuration and Package.resolved

--- FlightSeatSelector/FlightSeatSelector/AppDelegate.swift
+++ FlightSeatSelector/FlightSeatSelector/AppDelegate.swift
@@ -7,20 +7,70 @@
 
 import UIKit
 import UIKitCore
+import SwiftCore
+import JSON
+import HTTPCore
+import URLSessionHTTPClient
+import Telme
+import TelmeSinks
 
 @main
 class AppDelegate: CheckpointedAppDelegate {
+	// MARK: + Private scope
+
+	private var telmeRecordSink: TelmeRecordSink!
+
+	// MARK: + Default scope
+
 	override func initialize() {
 		super.initialize()
+
+		let baselineWallNanos = Int(truncatingIfNeeded: timeInfo.baselineWall.unixEpochNanoseconds)
+		let baselineMonotonicNanos = Int(truncatingIfNeeded: timeInfo.baselineMonotonic.nanoseconds)
+
+		let sessionJSON: [String: JSON] = [
+			"session_id": .string(sessionId.uuidString),
+
+			"bundle_id": .string(appInfo.bundleId),
+			"app_version": .string(appInfo.appVersion),
+			"install_id": .string(appInfo.installId.uuidString),
+
+			"device_os": .string(deviceInfo.osName),
+			"device_os_version": .string(deviceInfo.osVersion),
+			"device_hardware_model": .string(deviceInfo.hardwareModel),
+			"device_manufacturer": .string(deviceInfo.manufacturer),
+
+			"baseline_wall_nanos": .int(baselineWallNanos),
+			"baseline_mono_nanos": .int(baselineMonotonicNanos),
+			"timezone_offset_sec": .int(Int(timeInfo.timezoneOffsetSeconds)),
+		]
+
+		if let url = URL(string: "https://sees-investigated-ham-nations.trycloudflare.com/telme/ingest") {
+			let clickHouseSink = ClickHouseTelmeSink(
+				http: URLSessionHTTPClient(session: .shared),
+				config: .init(
+					endpoint: url,
+					session: sessionJSON
+				)
+			)
+
+			Telme.default.addRecordSink(clickHouseSink)
+			telmeRecordSink = clickHouseSink
+		}
 	}
 
 	override func app(
 		_ application: UIApplication,
 		didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
 	) -> Bool {
+		let superResult = super.app(
+			application,
+			didFinishLaunchingWithOptions: launchOptions
+		)
+
 		if #available(iOS 13.0, *) {
-			// window created by SceneDelegate
-			return true
+			// window will be created by SceneDelegate
+			return superResult
 		}
 
 		// iOS 12: create window here
@@ -28,10 +78,7 @@ class AppDelegate: CheckpointedAppDelegate {
 		window?.rootViewController = ViewController()
 		window?.makeKeyAndVisible()
 
-		return super.app(
-			application,
-			didFinishLaunchingWithOptions: launchOptions
-		)
+		return superResult
 	}
 
 	@available(iOS 13.0, *)
